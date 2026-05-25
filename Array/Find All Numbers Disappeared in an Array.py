144aeebcf6d9ba3a1f03941af3233d6aa48b1a3a
Arisha-chadha <arishachadha13@gmail.com>
2026-05-25 21:33:21 +0530

    Update Find All Numbers Disappeared in an Array.py

--- Array/Find All Numbers Disappeared in an Array.py
+++ Array/Find All Numbers Disappeared in an Array.py
@@ -1,4 +1,4 @@
-#O(n) time complexity 
+#O(n) time complexity space complexity 0(n)
 class Solution(object):
     def findDisappearedNumbers(self, nums):
         seen=set(nums)
@@ -7,4 +7,16 @@ class Solution(object):
             if i not in seen:
                 ans.append(i)
         return ans
+
+#optimal O(n) time complexity space complexity 0(1)
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        for i in range(len(nums)):
+            index= abs(nums[i])-1
+            nums[index]=-abs(nums[index])
+        ans=[]
+        for i in range(len(nums)):
+            if nums[i]> 0 :
+                ans.append(i+1)
+        return ans 
         
