63356bc15e40668de1f0a9e2fc5c71573319a40f
Arisha-chadha <arishachadha13@gmail.com>
2026-05-31 21:33:25 +0530

    Create Destroying Asteroids.py

---
+++ Array/Destroying Asteroids.py
@@ -0,0 +1,11 @@
+#Time complexity O(nlogn)
+class Solution(object):
+    def asteroidsDestroyed(self, mass, asteroids):
+        asteroids.sort()
+        for i in asteroids:
+            if mass>= i:
+                mass+= i
+            else :
+                return False 
+        return True 
+        
